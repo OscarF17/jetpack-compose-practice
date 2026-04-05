75c04d7e0927a6d956007c98b517f3a771760883
Oscar Flores <oscar.l.flores@oracle.com>
2026-04-04 23:46:39 -0600

    added slider with label

--- TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
@@ -23,16 +26,26 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberSliderState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.fromColorLong
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,6 +53,7 @@ import com.example.tipcalculator.ui.theme.TipCalculatorTheme
 
 val purple = Color(0xFF6911C6)
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -52,6 +66,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize())
                 { innerPadding ->
                     val tip = rememberTextFieldState(initialText = "")
+                    var percentage = rememberSliderState(
+                        value = 10f,
+                        valueRange = 0f..100f,
+                        steps = 19
+                    )
                     Column(
                         modifier = Modifier
                             .padding(innerPadding)
@@ -69,6 +88,23 @@ class MainActivity : ComponentActivity() {
                             ) {
                                 Text(text = "Reset")
                             }
+                        }
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Slider(
+                                state = percentage,
+                                enabled = true,
+                                modifier = Modifier.weight(1f),
+                                // TODO: Update slider to a ball
+                            )
+                            Spacer(modifier = Modifier.padding(10.dp))
+                            Text(
+                                text = percentage.value.toInt().toString() + "%",
+                                fontSize = 30.sp,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.width(75.dp).border(width = 1.dp, color = Color.Black),
+                            )
                         }
                     }
                 }
@@ -99,8 +135,8 @@ fun TitleBar(titleText:String, modifier: Modifier = Modifier) {
 fun TipTextField(tip: TextFieldState, modifier: Modifier = Modifier) {
     OutlinedTextField(
         state = tip,
-        placeholder = { Text(text = "Insert tip") },
-        prefix = { Text(text = "$")},
+        placeholder = { Text(text = "Insert bill") },
+        prefix = { Text(text = "$", modifier = Modifier.padding(end = 5.dp))},
         lineLimits = TextFieldLineLimits.SingleLine,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
         inputTransformation = {
