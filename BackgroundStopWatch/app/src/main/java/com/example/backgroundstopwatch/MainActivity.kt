05dc7afa9bc7f9d996fadb287cd0830932015196
Oscar Flores <oscarlflores@protonmail.com>
2026-04-24 22:44:55 -0600

    make stopwatch functional

--- BackgroundStopWatch/app/src/main/java/com/example/backgroundstopwatch/MainActivity.kt
+++ BackgroundStopWatch/app/src/main/java/com/example/backgroundstopwatch/MainActivity.kt
@@ -4,26 +4,48 @@ import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
+import com.example.backgroundstopwatch.service.StopwatchService
 import com.example.backgroundstopwatch.ui.theme.BackgroundStopWatchTheme
+import com.example.backgroundstopwatch.utils.Constants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        createNotificationChannel()
-        // In 2026, we check this immediately for background apps
+        enableEdgeToEdge()
+        requestNotificationPermission()
+        setContent {
+            BackgroundStopWatchTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    StopwatchScreen()
+                }
+            }
+        }
+    }
+    private fun requestNotificationPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             ActivityCompat.requestPermissions(
                 this,
@@ -31,50 +53,43 @@ class MainActivity : ComponentActivity() {
                 0
             )
         }
-        enableEdgeToEdge()
-        setContent {
-            BackgroundStopWatchTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+    }
+}
+
+@Composable
+fun StopwatchScreen() {
+    val context = LocalContext.current
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Button(
+            onClick = {
+                val intent = Intent(context, StopwatchService::class.java).apply {
+                    action = Constants.ACTION_START
                 }
+
+                // In Android 8.0+, we use startForegroundService
+                context.startForegroundService(intent)
             }
+        ) {
+            Text("Start service")
         }
-    }
+        Spacer(modifier = Modifier.height(12.dp))
 
-    private fun createNotificationChannel() {
-        // 1. Define the basics
-        val name = "Stopwatch Channel"
-        val descriptionText = "Shows the active stopwatch timer"
-        val importance = NotificationManager.IMPORTANCE_LOW // Low = No sound/popup, perfect for a timer
-        val channelId = "stopwatch_channel"
-
-        // 2. Create the channel object
-        val channel = NotificationChannel(channelId, name, importance).apply {
-            description = descriptionText
+        // Stop Button
+        OutlinedButton(
+            modifier = Modifier.fillMaxWidth(0.6f),
+            onClick = {
+                val intent = Intent(context, StopwatchService::class.java).apply {
+                    action = Constants.ACTION_STOP
+                }
+                context.startService(intent)
+            }
+        ) {
+            Text("Stop Service")
         }
-
-        // 3. Register the channel with the system
-        val notificationManager: NotificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BackgroundStopWatchTheme {
-        Greeting("Android")
     }
 }
--- BackgroundStopWatch/app/src/main/java/com/example/backgroundstopwatch/service/StopwatchService.kt
+++ BackgroundStopWatch/app/src/main/java/com/example/backgroundstopwatch/service/StopwatchService.kt
@@ -3,16 +3,76 @@ package com.example.backgroundstopwatch.service
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.example.backgroundstopwatch.utils.Constants
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class StopwatchService: Service() {
+    // 1. Define a scope that lives as long as the service
+    private val serviceScope = CoroutineScope(Dispatchers.Main + Job())
+    private var timerJob: Job? = null
+    private var secondsElapsed = 0
+
     // This is not a "Bound" service, so we return null
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // This is where the magic happens when we call startService()
+        when (intent?.action) {
+            Constants.ACTION_START -> startStopwatchService()
+            Constants.ACTION_STOP -> stopService()
+        }
 
-        // START_STICKY tells Android: "If you kill me, restart me!"
+        // Automatically restart service if is killed automatically by Android
         return START_STICKY
+    }
+
+    private fun startStopwatchService() {
+        // Start as foreground service so it's not killed
+        startForeground(Constants.NOTIFICATION_ID, buildNotification("00:00:00"))
+
+        // Start the timer loop
+        timerJob?.cancel() // Reset if already running
+        timerJob = serviceScope.launch {
+            while (isActive) {
+                delay(1000)
+                secondsElapsed++
+                updateNotification(formatTime(secondsElapsed))
+            }
+        }
+    }
+
+    private fun updateNotification(timeText: String) {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as android.app.NotificationManager
+        notificationManager.notify(Constants.NOTIFICATION_ID, buildNotification(timeText))
+    }
+
+    // Create the notification that will be shown in the tray
+    private fun buildNotification(content: String): android.app.Notification {
+        return NotificationCompat.Builder(this, Constants.CHANNEL_ID)
+            .setContentTitle("Stopwatch Running")
+            .setContentText("Elapsed Time: $content")
+            .setSmallIcon(android.R.drawable.ic_media_play)
+            .setOngoing(true)
+            .setOnlyAlertOnce(true) // Crucial: prevents the phone from vibrating every second
+            .build()
+    }
+
+    private fun formatTime(seconds: Int): String {
+        val hours = seconds / 3600
+        val minutes = (seconds % 3600) / 60
+        val secs = seconds % 60
+        return String.format("%02d:%02d:%02d", hours, minutes, secs)
+    }
+
+    private fun stopService() {
+        timerJob?.cancel()
+        stopForeground(STOP_FOREGROUND_REMOVE)
+        stopSelf()
     }
 
     override fun onDestroy() {
